e563f9aa35b343480ef6315f1a926b7086666057
Alain Keller <alain.keller@edu.tbz.ch>
2015-09-01 19:55:32 +0200

    Added Knight

---
+++ Schach/ChessPieces/knight.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess.ChessPieces
+{
+    class Knight : ChessPiece
+    {
+        protected override bool CanEat()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool CanMoveThere(int row, int column)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Eat()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void GetEaten()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool TryEat(int rown, int column)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
