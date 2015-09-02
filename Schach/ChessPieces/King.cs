b8a66b21c2e8b906ffe1af9b0926f330cce90ee7
Pascal Honegger <pascal.honegger.external@atos.net>
2015-09-02 18:09:03 +0200

    Commit Before Merge

---
+++ Schach/ChessPieces/King.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess.ChessPieces
+{
+    class King : ChessPiece
+    {
+        public King(int row, int column, bool isWhite) : base(row, column, isWhite)
+        {
+        }
+
+        protected override bool CanMoveThere(int row, int column)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool CanEat(int row, int column)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
