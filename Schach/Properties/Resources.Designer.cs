58b553063e9e7db63559260f919d322bc99ae5f2
Pascal Honegger <pascal.honegger.external@atos.net>
2015-09-02 12:47:28 +0200

    Added 2 Test-Resources for the Graveyard (Really Ugly). But this shows that resources can be inserted!

--- Schach/Properties/Resources.Designer.cs
+++ Schach/Properties/Resources.Designer.cs
@@ -59,5 +59,25 @@ namespace Chess.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap GraveYard {
+            get {
+                object obj = ResourceManager.GetObject("GraveYard", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap GraveYardImage {
+            get {
+                object obj = ResourceManager.GetObject("GraveYardImage", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
