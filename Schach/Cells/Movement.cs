f535c5a36a6b6cb1a8da0aad5dbb02eb8d8e1d31
Pascal Honegger <pascal.honegger@edu.tbz.ch>
2015-11-03 10:49:08 +0100

    Imporved Lernournal, added some Comments.

--- Schach/Cells/Movement.cs
+++ Schach/Cells/Movement.cs
@@ -1,7 +1,13 @@
 namespace Chess.Cells
 {
+	/// <summary>
+	/// Class that holds the important Enum Direction
+	/// </summary>
 	public static class Movement
 	{
+		/// <summary>
+		/// Enum used for the orientation insade the chessboard
+		/// </summary>
 		public enum Direction
 		{
 			Top,
